b8d6fb0ce473402fdb601c97b6592f3a0b242c79
tanase-j-ww <tanase.j@world-wing.com>
2024-09-28 22:04:39 +0900

    🎨 Task schema

---
+++ src/schemas/task_schema.ts
@@ -0,0 +1,73 @@
+import { z } from "@hono/zod-openapi";
+
+const taskIdSchama = z.string().openapi({
+  title: "タスクのID",
+  description: "タスクのIDです。",
+  example: "9aa8caf4ca0c4963a9a035d76a3018fe",
+});
+
+const titleSchema = z.string().openapi({
+  title: "タスクのタイトル",
+  description: "タスクのタイトルです。",
+  example: "TASK1",
+});
+
+const descriptionSchema = z.string().max(255).openapi({
+  title: "タスクの詳細",
+  description: "タスクの詳細です。",
+  example: "わからないことがあれば上司に確認",
+});
+
+const dueDateSchema = z.date().openapi({
+  title: "タスクの期日",
+  description: "タスクの期日です。",
+  example: "2022-12-31",
+});
+
+const prioritySchema = z.enum(["High", "Medium", "Low"]).openapi({
+  title: "タスクの優先度",
+  description: "タスクの優先度です。",
+  example: "High",
+});
+const isCompletedSchema = z.boolean().openapi({
+  title: "タスクの完了状態",
+  description: "タスクの完了状態です。",
+  example: false,
+});
+const isDeletedSchame = z.boolean().openapi({
+  title: "タスクの削除フラグ",
+  description: "タスクの削除フラグです。",
+  example: false,
+});
+const createdAtSchema = z.date().openapi({
+  title: "タスクの登録日",
+  description: "タスクの登録日です。",
+  example: "2022-12-01",
+});
+const updatedAtSchema = z.date().openapi({
+  title: "タスクの更新日",
+  description: "タスクの更新日です。",
+  example: "2022-12-02",
+});
+
+export const taskSchema = z.object({
+  id: taskIdSchama,
+  title: titleSchema,
+  description: descriptionSchema,
+  dueDate: dueDateSchema,
+  priority: prioritySchema,
+  isCompleted: isCompletedSchema,
+  isDeleted: isDeletedSchame,
+  createdAt: createdAtSchema,
+  updatedAt: updatedAtSchema,
+});
+
+export const tasksSchema = z.array(taskSchema);
+
+export const taskCreateRequestSchema = z.object({
+  title: titleSchema,
+  description: descriptionSchema,
+  dueDate: dueDateSchema,
+  priority: prioritySchema,
+  isCompleted: isCompletedSchema,
+});
